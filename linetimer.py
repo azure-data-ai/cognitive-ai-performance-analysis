49b213dd5fa525b315fdf8da35eb6b389ae2ce2e
Rahul <39583459+singh-rahul@users.noreply.github.com>
2021-05-18 20:07:58 +0800

    linetime process time disabled

--- linetimer.py
+++ linetimer.py
@@ -7,12 +7,13 @@ class CodeTimer:
 
     def __enter__(self):
         self.start = timeit.default_timer()
-        self.start_process = time.process_time()   ## added to calculate process-time
+       # self.start_process = time.process_time()   ## added to calculate process-time
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.took = (timeit.default_timer() - self.start) * 1000.0
-        print(self.name + ' Code block Total time: ' + str(self.took) + ' ms')
-        self.processtook = (time.process_time() - self.start_process) * 1000.0
-        print(self.name + ' Code block Process time: ' + str(self.processtook) + ' ms')
-        print(f'\x1b[6;30;42m {self.name} API Response time: {self.took - self.processtook} ms \x1b[0m')
+        # print(self.name + ' Code block Total time: ' + str(self.took) + ' ms')
+        print(f'\x1b[6;30;42m {self.name} Total Taken: {self.took} ms \x1b[0m')
+        # self.processtook = (time.process_time() - self.start_process) * 1000.0
+        # print(self.name + ' Code block Process time: ' + str(self.processtook) + ' ms')
+        # print(f'\x1b[6;30;42m {self.name} API Response time: {self.took - self.processtook} ms \x1b[0m')
 
